0113a7d85f7f39ab94c937cfa555b52e3f028d67
Dilum Aluthge <dilum@aluthge.com>
2020-07-17 20:36:20 -0400

    Add the initial functionality (#2)
    * Update Project.toml
    
    * Update CleanUpPullRequestPreviews.jl
    
    * Update README.md

--- README.md
+++ README.md
@@ -4,3 +4,45 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://bcbi.github.io/CleanUpPullRequestPreviews.jl/dev)
 [![Build Status](https://github.com/bcbi/CleanUpPullRequestPreviews.jl/workflows/CI/badge.svg)](https://github.com/bcbi/CleanUpPullRequestPreviews.jl/actions)
 [![Coverage](https://codecov.io/gh/bcbi/CleanUpPullRequestPreviews.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/bcbi/CleanUpPullRequestPreviews.jl)
+
+CleanUpPullRequestPreviews automatically deletes old pull request previews from Documenter, Franklin, etc.
+
+## Example usage
+
+### Step 1:
+
+Create a file named `.ci/Project.toml` with the following contents:
+```toml
+[deps]
+CleanUpPullRequestPreviews = "06c59129-2005-40e8-8e6e-18d91e04568a"
+
+[compat]
+CleanUpPullRequestPreviews = ">= 0.1"
+```
+
+### Step 2:
+
+Make sure that you have a deploy key with write permissions on `JuliaHealth/juliahealth.github.io-previews`, and the private key for this deploy key is available as a secret named `FRANKLIN_PRIV_PREVIEW`.
+
+Create a file named `.github/workflows/CleanUpPullRequestPreviews.yml` with the following contents:
+```yaml
+name: CleanUpPullRequestPreviews
+
+on:
+  schedule:
+    - cron: '00 00 * * *' # run one time per day
+
+jobs:
+  CleanUpPullRequestPreviews:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Install SSH Client (pull request previews)
+        uses: webfactory/ssh-agent@v0.2.0
+        with:
+          ssh-private-key: ${{ secrets.FRANKLIN_PRIV_PREVIEW }}
+      - run: julia --project=.ci/ -e 'using Pkg; Pkg.instantiate()'
+      - name: CleanUpPullRequestPreviews.remove_old_previews
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        run: julia -e 'using CleanUpPullRequestPreviews; CleanUpPullRequestPreviews.remove_old_previews(; repo_main = "JuliaHealth/juliahealth.github.io", repo_previews = "git@github.com:JuliaHealth/juliahealth.github.io-previews.git", repo_previews_branch = "gh-pages")'
+```
--- src/CleanUpPullRequestPreviews.jl
+++ src/CleanUpPullRequestPreviews.jl
@@ -1,5 +1,178 @@
 module CleanUpPullRequestPreviews
 
-# Write your package code here.
+import GitHub
+import HTTP
 
+export remove_old_previews
+
+struct PRandPath
+    pr::Int 
+    path::String
 end
+
+struct Config{A <: GitHub.Authorization}
+    auth::A
+    email_address::String
+    git_command::String
+    my_regex::Regex
+    num_samples::Int
+    repo_main::String
+    repo_previews::String
+    repo_previews_branch::String
+    sample_delay_seconds::Int
+    username::String
+end
+
+function Config(;
+                auth::GitHub.Authorization = GitHub.authenticate(ENV["GITHUB_TOKEN"]),
+                email_address::AbstractString = "41898282+github-actions[bot]@users.noreply.github.com",
+                git_command::AbstractString = "git",
+                my_regex::Regex = r"^\.\/previews\/PR(\d*)$",
+                num_samples::Integer = 3,
+                repo_main::AbstractString,
+                repo_previews::AbstractString,
+                repo_previews_branch::AbstractString,
+                sample_delay_seconds::Integer = 60,
+                username::AbstractString = "github-actions[bot]",
+                )
+    result = Config(
+        auth,
+        email_address,
+        git_command,
+        my_regex,
+        num_samples,
+        repo_main,
+        repo_previews,
+        repo_previews_branch,
+        sample_delay_seconds,
+        username,
+    )
+    return result
+end
+
+"""
+    remove_old_previews
+
+Remove old pull request previews.
+"""
+function remove_old_previews(;
+                             api::GitHub.GitHubAPI = GitHub.GitHubWebAPI(HTTP.URI("https://api.github.com")),
+                             kwargs...)
+    config = Config(; kwargs...)
+
+    clone_directory = _git_clone(api, config)::String
+    prs_and_paths = _get_prs_and_paths(api, config, clone_directory)::Vector{PRandPath}
+    pr_is_open = _pr_is_open(api, config, prs_and_paths)::Dict{Int, Bool}
+    _delete_preview_directories(api, config, clone_directory, prs_and_paths, pr_is_open)::Nothing
+    _git_add_commit_push(api, config, clone_directory)::Nothing
+    return nothing
+end
+
+
+function _git_clone(api::GitHub.GitHubAPI,
+                    config::Config)
+    parent_dir = mktempdir()
+    atexit(() -> rm(parent_dir; force = true, recursive = true))
+    original_directory = pwd()
+    cd(parent_dir)
+    run(`$(config.git_command) clone $(config.repo_previews) CLONEDIRECTORY`)
+    clone_directory = joinpath(parent_dir, "CLONEDIRECTORY")
+    cd(clone_directory)
+    run(Cmd(String[config.git_command, "config", "user.name", strip(config.username)]))
+    run(Cmd(String[config.git_command, "config", "user.email", strip(config.email_address)]))
+    run(`$(config.git_command) checkout $(config.repo_previews_branch)`)
+    cd(original_directory)
+    return clone_directory
+end
+
+function _get_prs_and_paths(api::GitHub.GitHubAPI,
+                            config::Config,
+                            root_directory::AbstractString)
+    result = Vector{PRandPath}(undef, 0)
+    original_directory = pwd()
+    cd(root_directory)
+    for (root, dirs, files) in walkdir(".")
+        for dir in dirs
+            path = joinpath(root, dir)
+            m = match(config.my_regex, path)
+            if m !== nothing
+                pr = parse(Int, m[1])
+                push!(result, PRandPath(pr, path))
+            end
+        end
+    end
+    cd(original_directory)
+    return result
+end
+
+function _pr_is_open(api::GitHub.GitHubAPI,
+                     config::Config,
+                     prs_and_paths)
+    auth = config.auth
+    num_samples = config.num_samples
+    sample_delay_seconds = config.sample_delay_seconds
+
+    intermediate = Vector{Dict{Int, Bool}}(undef, num_samples)
+    for i = 1:num_samples
+        intermediate[i] = Dict{Int, Bool}()
+    end
+    for i = 1:num_samples
+        @info("Waiting for $(sample_delay_seconds) second(s)...")
+        sleep(sample_delay_seconds)
+        for pr_and_path in prs_and_paths
+            pr = pr_and_path.pr 
+            @info("Getting state of PR # $(pr)")
+            github_pr = GitHub.pull_request(api,
+                                            config.repo_main,
+                                            pr;
+                                            auth = auth)
+            github_pr_is_open = github_pr.state == "open"
+            intermediate[i][pr] = github_pr_is_open
+        end
+    end
+    result = Dict{Int, Bool}()
+    for pr_and_path in prs_and_paths
+        pr = pr_and_path.pr 
+        pr_intermediates = Vector{Bool}(undef, num_samples)
+        for i = 1:num_samples
+            pr_intermediates[i] = intermediate[i][pr]
+        end
+        result[pr] = any(pr_intermediates)
+        @info("PR # $(pr) is open: $(result[pr])")
+    end 
+    return result
+end
+
+function _git_add_commit_push(api::GitHub.GitHubAPI,
+                              config::Config,
+                              clone_directory::AbstractString)
+    original_directory = pwd()
+    cd(clone_directory)
+    run(`git add -A`)
+    run(`git commit -m "Automated commit created by CleanUpPullRequestPreviews.jl"`)
+    # run(`git push origin --all`)
+    cd(original_directory)
+    return nothing
+end
+
+function _delete_preview_directories(api::GitHub.GitHubAPI,
+                                     config::Config,
+                                     clone_directory::AbstractString,
+                                     prs_and_paths::Vector{PRandPath},
+                                     pr_is_open::Dict{Int, Bool})
+    original_directory = pwd()
+    for pr_and_path in prs_and_paths
+        pr = pr_and_path.pr 
+        path = pr_and_path.path
+        this_pr_is_open = pr_is_open[pr]
+        if !this_pr_is_open
+            cd(clone_directory)
+            @info("Removing \"$(path)\"")
+            rm(path; force = true, recursive = true)
+        end
+    end 
+    cd(original_directory)
+    return nothing
+end
+
+end  # end module CleanUpPullRequestPreviews
